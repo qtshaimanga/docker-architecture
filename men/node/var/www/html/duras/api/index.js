da6fd8d8abd05c1bc878e6d34899eeea6e708a6d
qtshaimanga <q.tshaimanga@gmail.com>
2017-07-27 14:17:59 +0200

    up

--- men/node/var/www/html/duras/api/index.js
+++ men/node/var/www/html/duras/api/index.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(7000, function () {
+app.listen(7001, function () {
   console.log('Example app listening on port 7000!')
 })
