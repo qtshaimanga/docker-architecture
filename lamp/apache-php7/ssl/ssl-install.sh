3b8fcdf2471e89d792854a52a8976f5a08880984
qtshaimanga <q.tshaimanga@gmail.com>
2017-07-30 17:18:26 +0200

    up

--- lamp/apache-php7/ssl/ssl-install.sh
+++ lamp/apache-php7/ssl/ssl-install.sh
@@ -9,16 +9,17 @@ else
 
 	echo "DOMAINES are $DOMAINES"
 
-	for DOMAINE in $DOMAINES;
-	do
-
+	/opt/letsencrypt/letsencrypt-auto --noninteractive --apache --standalone certonly --renew-by-default --preferred-challenges tls-sni --email q.tshaimanga@gmail.com --agree-tos --domaines quentintshaimanga.fr
+	/usr/sbin/service apache2 reload
+	
+	# for DOMAINE in $DOMAINES;
+	# do
 		# /opt/letsencrypt/letsencrypt-auto --apache -m q.tshaimanga@gmail.com --agree-tos true -d $DOMAINE
 		#
 		# ssl-renewal $DOMAINE
 		# crontab -e 0 3 * * 1 /usr/local/sbin/ssl-renewal $DOMAINE >> /var/log/ssl-renewal.log
 
-		echo "$DOMAINE added"
-
-	done
+	# 	echo "$DOMAINE added"
+	# done
 
 fi
